6a12c5488f03ebf5302b482602084a05aa52e74b
Augusto Galego <augustomirandagalego@gmail.com>
2023-10-06 18:37:44 +0200

    .

--- vue-vs-react/react-app/src/App.js
+++ vue-vs-react/react-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>Count: {count}</p>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
         >
-          Learn React
-        </a>
+          +1
+        </button>
       </header>
-    </div>
   );
 }
 
