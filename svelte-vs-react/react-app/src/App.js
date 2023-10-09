d9d98ee28bc30a1f405c8a5d4c68445606fde831
Augusto Galego <augustomirandagalego@gmail.com>
2023-10-09 19:00:17 +0200

    .

--- svelte-vs-react/react-app/src/App.js
+++ svelte-vs-react/react-app/src/App.js
@@ -4,16 +4,13 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-      <header className="App-header">
-        <p>Count: {count}</p>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +1
-        </button>
-      </header>
+    <button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      current count: {count}
+    </button>
   );
 }
 
